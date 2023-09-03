4fa9c7eaa7ee3469f9ce3c792471bbfd92d11611
Stephen Collins <swcollins462@gmail.com>
2023-09-03 17:21:06 -0400

    admin panel and model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    publish_date = models.DateField(auto_now_add=True)
