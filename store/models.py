55976d5716ee12217f2456f6ac45df9d71430a19
Stephen Collins <swcollins462@gmail.com>
2023-09-04 18:51:08 -0400

    Data model and querying the database

--- store/models.py
+++ store/models.py
@@ -6,4 +6,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
-    publish_date = models.DateField(auto_now_add=True)
+    publish_date = models.DateField(default=datetime.date.today)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    stock = models.IntegerField(default=0)
