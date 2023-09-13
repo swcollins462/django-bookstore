8012d04512555976b1b3dac16d1ee87844c9fea0
Stephen Collins <swcollins462@gmail.com>
2023-09-13 11:07:58 -0400

    listing all books

--- store/views.py
+++ store/views.py
@@ -11,10 +11,20 @@ def index(request):
 
 
 def store(request):
-    count = Book.objects.all().count()
+    books = Book.objects.all()
+    count = books.count()
     context = {
         'count': count,
+        'books': books,
         'page': 'Welcome to Mystery Books',
         'menu_item': 'store',
     }
     return render(request, 'store/store.html', context)
+
+
+def display_book(request, book_id):
+    context = {
+        'page': 'TEST' + str(book_id),
+        'menu_item': 'TEST' + str(book_id),
+    }
+    return render(request, 'store/template.html', context)
